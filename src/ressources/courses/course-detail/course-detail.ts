c79dc27d1912a1ac8b556196926a115a670b3db6
Pixel Polo <paulr237@gmail.com>
2024-12-01 22:14:06 +0100

    course-api test fixed

--- src/ressources/courses/course-detail/course-detail.ts
+++ src/ressources/courses/course-detail/course-detail.ts
@@ -1,13 +1,24 @@
+import { AuthService } from "./../../../services/auth/auth";
 import { bindable, ICustomElementViewModel } from "aurelia";
 import { FullCourse } from "../../../services/api/course-api";
+import { resolve } from "aurelia";
 
 export class CourseDetail implements ICustomElementViewModel {
+  // ********************
+  // ***** SERVICES *****
+  // ********************
+
+  readonly authService: AuthService = resolve(AuthService);
+
   // ******************
   // ***** FIELDS *****
   // ******************
   @bindable() course: FullCourse;
 
+  public canEdit: boolean;
+
   attached() {
     console.log(this.course);
+    this.canEdit = this.authService.getUserRoles().includes("professor");
   }
 }
